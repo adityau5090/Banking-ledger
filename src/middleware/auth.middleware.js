cbfb33d26098f87a10e370ab8a75f3d8648fbf7b
Aaddityaaa <adityau5090@gmail.com>
2026-02-24 19:44:13 +0530

    update middleware to authenticate system user

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -19,8 +19,48 @@ async function authMiddleware(req,res,next) {
 
         return next();
     } catch (error) {
-        
+        console.error("Error in auth middleware : ", error);
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid",
+            error: error
+        })
     }
 }
 
-module.exports = {authMiddleware}
+async function authSystemUserMiddleware(req,res,next){
+    const token = req.cookies?.token || req.headers.authorization?.split("")[1];
+    
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized access, token is missing"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser");
+
+        if(!user.systemUser){
+            return res.status(403).json({
+                message: "Fobidden access, not a system user"
+            })
+        }
+
+        req.user = user
+
+        return next()
+
+    } catch (error) {
+        console.error("Error in authSystem middleware : ", error);
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid",
+            error: error
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware,
+    authSystemUserMiddleware
+}
